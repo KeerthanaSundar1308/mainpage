95dc9357e55b52713a5f3d1b0555dee8f5dc1bb8
Keerthana S <158754184+KeerthanaSundar1308@users.noreply.github.com>
2024-06-19 06:28:36 +0530

    initial commit

--- blogproject/src/App.jsx
+++ blogproject/src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Update from './components/Update';
 import Header from './components/Header';
-import Login from './components/Login';
-import Register from './components/Register';
+
 
 function App() {
    const [count, setCount] = useState(0)
@@ -12,9 +11,9 @@ function App() {
     <div className="text-white h-[100vh] flex justify-center items-center bg-cover"  
     style={{"background":"url('../src/assets/bg1.jpg"}}>
       
-      {/* <Header/>
-      <Update/> */}
-      <Login/>
+      <Header/>
+      <Update/>
+      {/* <Login/> */}
       
     </div>
   )
